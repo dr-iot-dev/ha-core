b1f6d81f931c30310848acb96c07d886dabb8064
dr-iot-dev <71931397+dr-iot-dev@users.noreply.github.com>
2024-11-14 04:38:30 +0000

    audio recorder test

---
+++ config/python_scripts/save_audio_data.py
@@ -0,0 +1,6 @@
+"""Save audio data to a file."""
+# # save_audio_data.py
+# data = hass.states.get("sensor.esphome_encoded_audio").state
+
+# # notify サービスを使ってエンコードされたデータをファイルに保存
+# hass.services.call("notify", "save_audio_data", {"message": data})
